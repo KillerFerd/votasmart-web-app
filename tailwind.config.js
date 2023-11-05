f6af5b47be1e5b91aff41e0892aa7e0d685f3fd1
gabriela-giron <krodriguezg4@miumg.edu.gt>
2023-11-05 16:31:06 -0600

    header y config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: [
+    "./src/**/*.{jsx,js}",
+    "./src/views/app/**/*.{jsx,js}"
+  ],
+  theme: {
+    colors: {
+      'titulo': '#265C4C',
+      'seleccion': '#ECBD19',
+      'fondo1': '#8FC1B5',
+      'fondo2': '#AFC5C0',
+      'fondo3': '#000000',
+      'hover': '#FFFFFF'
+    },
+    fontFamily: {
+      title: ['Vina Sans Regular','sans'],
+      subtitle: ['Sarabun Bold','sans'],
+      paragraph: ['Sarabun Regular','sans']
+    },
+    extend: {
+      fontSize: {
+        'sm': '14px',
+        'base': '24px',
+        'lg': '72px',
+        'xl': '120px',
+      },
+      margin: {
+        '0': '0',
+        auto: 'auto',
+      },
+      padding: {
+        '0': '0',
+      },
+      borderRadius: {
+        'base': '12px',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')({
+      strategy: 'class' // only generate classes
+    }),
+    require('@tailwindcss/container-queries'),
+  ],
+}
+
+
