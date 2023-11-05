651f70b33dd214b75e0f59f2db0f7fed619cd8f7
gabriela-giron <krodriguezg4@miumg.edu.gt>
2023-11-05 17:05:15 -0600

    footer y cambio de nombre de pestaña

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/header'
+import Footer from './components/footer'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   )
 }
