ddec5527f2d816d2c5c4ced3f2449c716b612596
KillerFerd <91443717+KillerFerd@users.noreply.github.com>
2023-11-06 19:27:01 -0600

    Commit: Cuartos de Final

--- src/features/App/CuartosFinal/CuartosFinal.jsx
+++ src/features/App/CuartosFinal/CuartosFinal.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import CalendarNav from '../../../components/CalendarNav/CalendarNav'
+import Container from '../../../components/Container/Container'
+import Text from '../../../components/Text/Text'
+import VsTarjet from '../../../components/VsTarjet/VsTarjet'
+import * as Styles from './Styles'
 
 const CuartosFinal = () => {
   return (
     <>
       <CalendarNav />
+      <Container>
+        <Styles.Section1>
+          <Text textType={'title'}>CUARTOS DE FINAL</Text>
+        </Styles.Section1>
+        <Styles.Section2>
+          <Text textType={'subTitle'}>Clasificación 1</Text>
+          <VsTarjet partido={'Partido 1'} />
+          <VsTarjet partido={'Partido 2'} />
+          <Text textType={'subTitle'}>Clasificación 2</Text>
+          <VsTarjet partido={'Partido 1'} />
+          <VsTarjet partido={'Partido 2'} />          
+          <Text textType={'subTitle'}>Clasificación 3</Text>
+          <VsTarjet partido={'Partido 1'} />
+          <VsTarjet partido={'Partido 2'} />                    
+          <Text textType={'subTitle'}>Clasificación 3</Text>
+          <VsTarjet partido={'Partido 1'} />
+          <VsTarjet partido={'Partido 2'} />                      
+        </Styles.Section2>
+      </Container>
     </>
   )
 }
