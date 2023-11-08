a56c1bbe5e15f128c359ad7bcdcb3a19b0b88d4e
Rodriguez Giron, Keren Gabriela <kgrodriguez@bi.com.gt>
2023-11-08 13:58:56 -0600

    agregar pantallas de torneo

--- src/features/Admin/AgregarEquipo/AgregarEquipo.jsx
+++ src/features/Admin/AgregarEquipo/AgregarEquipo.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import Addinfo from '../../../assets/plus-circle.svg'
 
 const AgregarEquipo = () => {
   return (
-    <div>AgregarEquipo</div>
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-4">Agregar Equipo</h1>
+      <section className="flex flex-col items-center">
+        <button className="bg-transparent hover:bg-seleccion hover:text-hover font-paragraph py-2 px-4 rounded-base">
+          Agregar presidente
+        </button>
+        <div className="flex items-center mt-4">
+          <input type="text" className="border border-1 py-2 px-4 rounded-base mr-2" placeholder="Nombre del presidente" />
+          <img src={Addinfo} alt="Info icon" />
+        </div>
+      </section>
+      <table className="table-auto border-separate rounded-base">
+        <thead>
+          <tr>
+            <th className="border border-1 px-4 py-2">No.</th>
+            <th className="border border-1 px-4 py-2">Equipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-1 px-4 py-2">1</td>
+            <td className="border border-1 px-4 py-2">Equipo 1</td>
+            <img src={Addinfo} alt="Info icon" />
+          </tr>
+          <tr>
+            <td className="border border-1 px-4 py-2">2</td>
+            <td className="border border-1 px-4 py-2">Equipo 2</td>
+            <img src={Addinfo} alt="Info icon" />
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- src/features/Admin/DetalleTorneo/DetalleTorneo.jsx
+++ src/features/Admin/DetalleTorneo/DetalleTorneo.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import info from "../../../assets/info.png"
+import deleted from '../../../assets/x.svg'
+import { Link } from 'react-router-dom';
 
 const DetalleTorneo = () => {
   return (
     <div className="w-full px-32 mt-12 flex flex-col justify-center items-center">
-        <h1 className='font-title text-lg text-center text-titulo'>Agregar Torneo Nuevo</h1>
+        <h1 className='font-title text-lg text-center text-titulo'>Nombre Torneo</h1>
         <div className="max-w-screen-xl mx-auto p-4 flex flex-col items-center">
           <form className="flex flex-wrap gap-4 pl-[12rem]">
           <div className="w-full md:w-1/2">
@@ -87,9 +90,42 @@ const DetalleTorneo = () => {
           </form>
           <div className="flex items-end w-full md:w-1/6">
               <button className="w-full bg-white border-2 text-fondo3 border-black hover:bg-seleccion hover:text-hover transition duration-300 px-6 py-1 my-8 rounded-base">
-                Agregar
+                Auto Asignar Equipos
+              </button>
+              <button className="w-full bg-white border-2 text-fondo3 border-black hover:bg-seleccion hover:text-hover transition duration-300 px-6 py-1 my-8 rounded-base">
+                Iniciar Torneo
+              </button>
+              <button className="w-full bg-white border-2 text-fondo3 border-black hover:bg-seleccion hover:text-hover transition duration-300 px-6 py-1 my-8 rounded-base">
+                Editar
               </button>
           </div>
+        </div>
+        <div>
+          <h1 className="font-subtitle text-lg text-center text-titulo">Equipos del Torneo</h1>
+          <button className="w-full bg-white border-2 text-fondo3 border-black hover:bg-seleccion hover:text-hover transition duration-300 px-6 py-1 my-8 rounded-base">
+            Agregar Equipo
+          </button>
+          <table className="border-separate w-full border-spacing-2">
+            <thead className='font-paragraph'>
+              <tr>
+                <th>No.</th>
+                <th>Equipo</th>
+                <th>Presidente</th>
+                <th>Nómina Jugadores</th>
+                <th>Nómina Técnicos</th>
+              </tr>
+              <tbody>
+                <tr>
+                  <td className="border-2 p-2 rounded-base">Jill</td>
+                  <td className="border-2 p-2 rounded-base">Smith</td>
+                  <td className="border-2 p-2 rounded-base">50</td>
+                  <img src={info} alt="info" className='w-6 h-auto' />
+                  <img src={info} alt="info" className='w-6 h-auto' />
+                  <img src={deleted} alt="" />
+                </tr> 
+              </tbody>
+            </thead>
+          </table>
         </div>
     </div>
   )
